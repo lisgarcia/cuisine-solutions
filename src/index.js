4d72df4acd31f5742652811d0a5fc9fd0ead48a9
Elisabeth Garcia <lisgarciae@gmail.com>
2023-05-14 16:05:42 -0400

    corrected

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 //cook times are in minutes
-//highest number in range chosen i.e. frozen cook time: 48-50 minutes. frozen cook time = 50 minutes.
 
-//did not include sous vide ultimate short rib dinner 
-//72 hour short ribs, creamy polenta and morel sauce, skipped meal bundle for two, together again bbq bundle
+
+//did not include sous vide ultimate short rib dinner, 72 hour short ribs, creamy polenta and morel sauce, skipped meal bundle for two, together again bbq bundle

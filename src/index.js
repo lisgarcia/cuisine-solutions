40cca81b748db2e8c4cdf9f019494b18e2c82bbe
Elisabeth Garcia <lisgarciae@gmail.com>
2023-05-15 13:33:08 -0400

    removed src/index.js

--- src/index.js
+++
@@ -1,4 +0,0 @@
-//cook times are in minutes
-
-
-//did not include sous vide ultimate short rib dinner, 72 hour short ribs, creamy polenta and morel sauce, skipped meal bundle for two, together again bbq bundle

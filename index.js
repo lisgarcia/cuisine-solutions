bf4105313f3c668e07de4b142a9f23c419fc41f2
Mai <garcia.ton94@gmail.com>
2023-05-15 10:39:50 -0400

    add more css to style product card, update db.json

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const productCardClass = ["flex", "justify-center", "items-center", "bg-white", "shadow-lg", "hover:shadow-xl", "p-5", "text-gray", "rounded-lg", "cursor-pointer"]
 const products = document.querySelector('#products')
 
+//Render Products
+
 fetch('http://localhost:3000/meals') 
     .then(resp => resp.json())
     .then(meals => meals.forEach(meal => renderMeal(meal)))
@@ -10,9 +12,32 @@ const renderMeal = (meal) => {
     productCard.id = "product-card";
     productCard.classList.add(...productCardClass);
     
+    const imgDiv = document.createElement('div');
+    imgDiv.classList.add('product-img');
+    const image = document.createElement('img');
+    image.src = meal.img;
+    imgDiv.append(image);
+    productCard.append(imgDiv);
+
+    const nameDiv = document.createElement('div');
+    nameDiv.classList.add('product-name');
+
+
     const name = document.createElement('h2');
     name.textContent = meal.name;
-    productCard.append(name);
+    nameDiv.append(name);
+    productCard.append(nameDiv);
 
     products.appendChild(productCard);
+
+    const buttonDiv = document.createElement('div');
+    buttonDiv.classList.add('favorite');
+    const favoriteBtn = document.createElement('button');
+    favoriteBtn.textContent = "Add to Favorite";
+    buttonDiv.append(favoriteBtn);
+    productCard.append(buttonDiv);
 }
+
+//Selected Products Section
+//create 1 div per column, 1st div would contain an image, second div, add this class col-span-2 to the second column 
+
